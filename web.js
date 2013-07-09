bfedea3226d4441fcf5ab70ef9646e0103dfebb0
packmapg <p@ck-man.com>
2013-07-09 22:38:24 +0000

    File

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
     buf = new Buffer(256);
-    response.send(buf.toString(fs.readFile("./index.html"))); 
+    response.send(buf.toString(
+	fs.readFile('./index.html')
+    ));			       
 });
 
 var port = process.env.PORT || 5000;
